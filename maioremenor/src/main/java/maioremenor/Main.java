8104179a1b665a3dd0f3f72b3ebf789d8e33d894
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-25 20:31:03 -0300

    Segundo Commit

--- maioremenor/src/main/java/maioremenor/Main.java
+++ maioremenor/src/main/java/maioremenor/Main.java
@@ -1,6 +1,17 @@
 package maioremenor;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int lista[] = new int[10];
+        System.out.println("Mostra o maior valor em uma lista com 10 valores inseridos pelo usuário");
+
+        for (int i = 0; i < lista.length; i++)
+        {
+            System.out.println("Insira um número: ");
+            int numero = input.nextInt();
+            lista[i] = numero;
+        }
+
     }
 }
